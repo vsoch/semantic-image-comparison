9ff6a774a1783db7101fe034d687940fb9159ead
vsoch <vsochat@stanford.edu>
2016-01-02 16:58:09 -0800

    modified:   preprocess/run_preprocess.sh

--- preprocess/run_preprocess.sh
+++ preprocess/run_preprocess.sh
@@ -8,7 +8,10 @@ base=/scratch/users/vsochat/DATA/BRAINMETA/ontological_comparison
 mkdir .jobs
 mkdir .out
 
-# Step 0: obtaining NeuroVault images and saving data
+# Step 0: obtaining NeuroVault images, saving data, and spatial image comparison
 python 0.neurovault_comparison.py $base
+
+# Step 1. Cognitive Atlas Prepartion for Reverse Inference
+python 1.prep_reverse_inference.py $base
 
 
