3ad70f0b6c1d69ba13f3f8061df922ad747f5136
vsoch <vsochat@stanford.edu>
2016-01-19 11:01:37 -0800

    modified:   preprocess/4.compile_classification_concept.py

--- preprocess/4.compile_classification_concept.py
+++ preprocess/4.compile_classification_concept.py
@@ -19,11 +19,20 @@ total = 0
 correct = 0
 for i in range(0,len(scores)):
     print "Parsing score %s of %s" %(i,len(scores))
-    result = pickle.load(open(scores[i],"rb"))
+    single_result = pickle.load(open(scores[i],"rb"))
     total=total+2
-    correct=correct+result["number_correct"]
-    comparison_df = comparison_df.append(result["comparison_df"])
+    correct=correct+single_result["number_correct"]
+    comparison_df = comparison_df.append(single_result["comparison_df"])
 
+
+
+# Use file names for index
+index_names = []
+for score in scores:
+    fname = os.path.basename(score)
+    index_names = index_names + [fname,fname,fname,fname]
+
+comparison_df.index = index_names
 accuracy = correct/float(total)
 result = dict()
 result["comparison_df"] = comparison_df
@@ -32,3 +41,27 @@ result["correct"] = correct
 result["accuracy"] = accuracy
 pickle.dump(result,open("%s/classification_results_binary.tsv" %results,"wb"))
 
+
+# Parse results for weighted (ontology based) classification
+scores_folder = "%s/classification_weighted" %(base)
+scores = glob("%s/*.pkl" %scores_folder)
+
+# Let's save a big data frame of the prediction scores
+comparison_weighted = pandas.DataFrame(columns=["actual","predicted","cca_score"])
+
+total = 0
+correct = 0
+for i in range(0,len(scores)):
+    print "Parsing score %s of %s" %(i,len(scores))
+    single_result = pickle.load(open(scores[i],"rb"))
+    total=total+2
+    correct=correct+single_result["number_correct"]
+    comparison_weighted = comparison_weighted.append(single_result["comparison_df"])
+
+accuracy = correct/float(total)
+result = dict()
+result["comparison_df"] = comparison_weighted
+result["total"] = total
+result["correct"] = correct
+result["accuracy"] = accuracy
+pickle.dump(result,open("%s/classification_results_weighted.tsv" %results,"wb"))
