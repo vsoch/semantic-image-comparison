1317df32cbe2b49166647f90a679b05fce1baedd
vsoch <vsochat@stanford.edu>
2016-01-19 18:07:21 -0800

    adding script to run calculation of null

---
+++ preprocess/3.calculate_null.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+# Classification framework
+# for image1 in all images:
+#    for image2 in allimages:
+#        if image1 != image2:
+#            hold out image 1 and image 2, generate regression parameter matrix using other images
+#            generate predicted image for image 1 [PR1]
+#            generate predicted image for image 2 [PR2]
+#            classify image 1 as fitting best to PR1 or PR2
+#            classify image 2 as fitting best to PR1 or PR2
+
+from pybraincompare.compare.maths import calculate_correlation
+from pybraincompare.compare.mrutils import get_images_df
+from pybraincompare.mr.datasets import get_standard_mask
+from pybraincompare.mr.transformation import *
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from glob import glob
+import pickle
+import pandas
+import nibabel
+import sys
+import os
+
+image_pairs = sys.argv[1]
+node_folder = sys.argv[2]   
+output_file = sys.argv[3]
+labels_tsv = sys.argv[4]
+
+# image_pairs should be string of image pairs 1|2,3|4 for about 2000 (half) the image pairs
+# We will calculate an accuracy across the image set, and build a null distribution by doing
+# this procedure many times
+image_pairs = image_pairs.split(",")
+total = 0
+correct = 0
+
+# We will save a vector of 
+# Images by Concept data frame, our X
+X = pandas.read_csv(labels_tsv,sep="\t",index_col=0)
+
+# Node pickles
+node_pickles = glob("%s/*.pkl" %node_folder)
+
+# Get standard mask, 4mm
+standard_mask=get_standard_mask(4)
+
+# Load any group data to get image paths
+group = node_pickles[0]
+group = pickle.load(open(group,"rb"))
+
+# Change paths in group pickle to point to 4mm folder
+group["in"] = [x.replace("resampled_z","resampled_z_4mm") for x in group["in"]]
+group["out"] = [x.replace("resampled_z","resampled_z_4mm") for x in group["out"]]
+
+concepts = X.columns.tolist()
+
+# We will go through each voxel (column) in a data frame of image data
+mr = get_images_df(file_paths=group["in"] + group["out"],mask=standard_mask)
+image_paths = group["in"] + group["out"]
+image_ids_in = [int(os.path.basename(x).split(".")[0]) for x in group["in"]]
+image_ids_out = [int(os.path.basename(x).split(".")[0]) for x in group["out"]]
+image_ids = image_ids_in + image_ids_out
+mr.index = image_ids
+
+for image_pair in image_pairs:
+    
+    image1_holdout,image2_holdout = [int(x) for x in image_pair.split("|")]
+
+    # Get the labels for holdout images
+    holdout1Y = X.loc[image1_holdout,:]
+    holdout2Y = X.loc[image2_holdout,:]
+
+    # what we can do is generate a predicted image for a particular set of concepts (e.g, for a left out image) by simply multiplying the concept vector by the regression parameters at each voxel.  then you can do the mitchell trick of asking whether you can accurately classify two left-out images by matching them with the two predicted images. 
+
+    regression_params = pandas.DataFrame(0,index=mr.columns,columns=concepts)
+
+    for voxel in mr.columns:
+        train = [x for x in X.index if x not in [image1_holdout,image2_holdout] and x in mr.index]
+        Y = mr.loc[train,voxel].tolist()
+        Xtrain = X.loc[train,:] 
+        # Use regularized regression
+        clf = linear_model.ElasticNet(alpha=0.1)
+        clf.fit(Xtrain,Y)
+        regression_params.loc[voxel,:] = clf.coef_.tolist()
+
+    print "Making predictions for %s vs %s..." %(image1_holdout,image2_holdout)
+    # Use regression parameters to generate predicted images
+    concept_vector1 =  pandas.DataFrame(holdout1Y)
+    concept_vector2 =  pandas.DataFrame(holdout2Y)
+    predicted_nii1 =  regression_params.dot(concept_vector1)
+    predicted_nii2 =  regression_params.dot(concept_vector2)
+
+    # Turn into nifti images
+    nii1 = numpy.zeros(standard_mask.shape)
+    nii2 = numpy.zeros(standard_mask.shape)
+    nii1[standard_mask.get_data()!=0] = predicted_nii1[image1_holdout].tolist()
+    nii2[standard_mask.get_data()!=0] = predicted_nii2[image2_holdout].tolist()
+    nii1 = nibabel.Nifti1Image(nii1,affine=standard_mask.get_affine())
+    nii2 = nibabel.Nifti1Image(nii2,affine=standard_mask.get_affine())
+
+    # Turn the holdout image data back into nifti
+    actual1 = mr.loc[image1_holdout,:]
+    actual2 = mr.loc[image2_holdout,:]
+    actual_nii1 = numpy.zeros(standard_mask.shape)
+    actual_nii2 = numpy.zeros(standard_mask.shape)
+    actual_nii1[standard_mask.get_data()!=0] = actual1.tolist()
+    actual_nii2[standard_mask.get_data()!=0] = actual2.tolist()
+    actual_nii1 = nibabel.Nifti1Image(actual_nii1,affine=standard_mask.get_affine())
+    actual_nii2 = nibabel.Nifti1Image(actual_nii2,affine=standard_mask.get_affine())
+
+    # Make a dictionary to lookup images based on nifti
+    lookup = dict()
+    lookup[actual_nii1] = image1_holdout
+    lookup[actual_nii2] = image2_holdout
+    lookup[nii1] = image1_holdout
+    lookup[nii2] = image2_holdout
+
+    comparison_df = pandas.DataFrame(columns=["actual","predicted","cca_score"])
+    comparisons = [[actual_nii1,nii1],[actual_nii1,nii2],[actual_nii2,nii1],[actual_nii2,nii2]]
+    count=0
+    for comp in comparisons:
+        name1 = lookup[comp[0]]
+        name2 = lookup[comp[1]]
+        corr = calculate_correlation(comp,mask=standard_mask)
+        comparison_df.loc[count] = [name1,name2,corr] 
+        count+=1
+
+    #   actual  predicted  cca_score
+    #0    3186       3186   0.908997
+    #1    3186        420   0.485644
+    #2     420       3186   0.044668
+    #3     420        420   0.657109
+
+    # Calculate accuracy
+    acc1 = comparison_df[comparison_df.actual==image1_holdout]
+    acc2 = comparison_df[comparison_df.actual==image2_holdout]
+
+    # Did we predict image1 to be image1?
+    if acc1.loc[acc1.predicted==image1_holdout,"cca_score"].tolist()[0] > acc1.loc[acc1.predicted==image2_holdout,"cca_score"].tolist()[0]:
+        correct+=1
+
+    # Did we predict image2 to be image2?
+    if acc2.loc[acc2.predicted==image2_holdout,"cca_score"].tolist()[0] > acc2.loc[acc2.predicted==image1_holdout,"cca_score"].tolist()[0]:
+        correct+=1
+
+    total = total + 2
+
+# Once we finish, save the single value
+result = dict()
+result["total"] = total
+result["correct"] = correct
+result["accuracy"] = correct/float(total)
+pickle.dump(result,open(output_file,"wb"))
---
+++ preprocess/3.run_calculate_null.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+from random import shuffle
+from glob import glob
+import sys
+import numpy
+import pandas
+import os
+
+#base = sys.argv[1]
+base = "/scratch/users/vsochat/DATA/BRAINMETA/ontological_comparison"
+data = "%s/data" %base
+node_folder = "%s/likelihood" %data
+output_folder = "%s/classification_null" %base
+results = "%s/results" %base  # any kind of tsv/result file
+
+# Images by Concepts data frame
+labels_tsv = "%s/images_contrasts_df.tsv" %results
+images = pandas.read_csv(labels_tsv,sep="\t",index_col=0)
+
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
+# Generate a list of all possible pairs to randomly sample from
+image_pairs = []
+for image1_holdout in images.index.tolist():
+    for image2_holdout in images.index.tolist():
+        if (image1_holdout != image2_holdout) and (image1_holdout < image2_holdout):
+            image_pairs.append("%s|%s" %(image1_holdout,image2_holdout))
+
+#len(image_pairs)
+#4186
+
+# We want approximately 1/4 for each group
+group_size = int(numpy.floor(len(image_pairs)/4.0))
+
+# Here is a function for writing and running the job script
+def run_job(group_list,iter_number,group_number):
+    job_id = "%s_%s" %(iter_number,group_number)
+    output_file = "%s/%s_predict.pkl" %(output_folder,job_id)
+    group_list = ",".join(group_list)
+    if not os.path.exists(output_file):
+        filey = ".job/null_%s.job" %(job_id)
+        filey = open(filey,"w")
+        filey.writelines("#!/bin/bash\n")
+        filey.writelines("#SBATCH --job-name=%s\n" %(job_id))
+        filey.writelines("#SBATCH --output=.out/%s.out\n" %(job_id))
+        filey.writelines("#SBATCH --error=.out/%s.err\n" %(job_id))
+        filey.writelines("#SBATCH --time=2-00:00\n")
+        filey.writelines("#SBATCH --mem=64000\n")
+        filey.writelines("python 3.calculate_null.py %s %s %s %s %s" %(group_list, node_folder, output_file, labels_tsv))
+        filey.close()
+        os.system("sbatch -p russpold " + ".job/null_%s.job" %(job_id))
+
+# We want to start off with 750 iterations, meaning splitting ~4000 images into four groups, so we get 3000 jobs in queue at once, each job should take ~19 hours.
+for i in range(750):
+    image_choices = image_pairs[:]
+    shuffle(image_choices)
+    # Split into four groups of images
+    group1 = image_choices[0:group_size]
+    group2 = image_choices[group_size:group_size*2]
+    group3 = image_choices[group_size*2:group_size*3]
+    group4 = image_choices[group_size*3:]
+    run_job(group1,i,1)
+    run_job(group2,i,2)
+    run_job(group3,i,3)
+    run_job(group4,i,4)
