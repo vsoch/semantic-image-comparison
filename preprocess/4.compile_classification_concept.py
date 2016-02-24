dc9e461a43f2238b4ef3f95bc8101f89b449a5d6
vsoch <vsochat@stanford.edu>
2016-02-24 15:59:49 -0800

    updating confusion matrix with Null images removed

--- preprocess/4.compile_classification_concept.py
+++ preprocess/4.compile_classification_concept.py
@@ -46,6 +46,9 @@ binary_df = binary["comparison_df"]
 unique_images = [int(x) for x in binary_df.actual.unique().tolist()]
 confusion = pandas.DataFrame(0,index=unique_images,columns=unique_images)
 
+# We cannot evaluate images with NaN - meaning the predicted image vector was empty
+nanimages = []
+
 print "Generating confusion matrix..."
 for i in range(0,len(scores)):
     print "Parsing confusion matrix for %s of %s" %(i,len(scores))
@@ -53,18 +56,33 @@ for i in range(0,len(scores)):
     cdf = single_result["comparison_df"]
     actuals = cdf.actual.unique().tolist()
     cdf.index = cdf.predicted.tolist()
-    for actual in actuals:
-        predictions = cdf.cca_score[cdf.actual==actual]
-        predictions.sort_values(ascending=False,inplace=True)
-        predicted = int(predictions.index[0])
-        actual = int(actual)  
-        confusion.loc[actual,predicted] = confusion.loc[actual,predicted] + 1
+    if cdf["cca_score"].isnull().any():
+        predicted_nulls = cdf["predicted"][cdf["cca_score"].isnull()].unique()
+        for predicted_null in predicted_nulls:
+            if predicted_null not in nanimages:
+                nanimages.append(predicted_null)
+    else:
+        for actual in actuals:
+            predictions = cdf.cca_score[cdf.actual==actual]
+            predictions.sort_values(ascending=False,inplace=True)
+            predicted = int(predictions.index[0])
+            actual = int(actual)  
+            confusion.loc[actual,predicted] = confusion.loc[actual,predicted] + 1
+
+#nanimages
+# [8718.0, 2963.0, 8727.0, 116.0, 111.0]
+
+# Remove images that are not comparable
+for nanimage in nanimages:
+    confusion = confusion.drop(nanimage,axis=0)
+    confusion = confusion.drop(nanimage,axis=1)
+
+#88 left
 
 confusion.to_csv("%s/classification_confusion_binary_4mm.tsv" %results,sep="\t")
 # Normalize confusion 
 confusion = confusion.divide(confusion.sum(1).tolist())
 confusion.to_csv("%s/classification_confusion_binary_4mm_norm.tsv" %results,sep="\t")
-
 
 # We also want to output a format for a visualization
 # var data = [
@@ -101,16 +119,25 @@ scores = glob("%s/*.pkl" %scores_folder)
 
 comparison_null = []
 
+# Missing images expression to search for
+missing_expression = "|".join([str(int(x)) for x in nanimages])
+
 for i in range(0,len(scores)):
     print "Parsing score %s of %s" %(i,len(scores))
-    single_result = pickle.load(open(scores[i],"rb"))
-    comparison_null.append(single_result["accuracy"])
+    if not re.search(missing_expression,scores[i]):
+        single_result = pickle.load(open(scores[i],"rb"))
+        comparison_null.append(single_result["accuracy"])
+    else:
+        print "Skipping %s" %(scores[i])
 
 accuracy = numpy.mean(comparison_null)
 result = dict()
 result["comparison_null"] = comparison_null
 result["accuracy"] = accuracy
 pickle.dump(result,open("%s/classification_results_null_4mm.pkl" %results,"wb"))
+
+#>>> result["accuracy"]
+#0.46994355374921259
 
 # Do two sample T test against actual vs null accuracies
 result_weighted = pickle.load(open("%s/classification_results_weighted_4mm.pkl" %results,"rb"))
@@ -121,12 +148,50 @@ tstat_binary,pval_binary = ttest_1samp(comparison_null,result_binary["accuracy"]
 
 # Add to saved results
 result_weighted["tstat"] = tstat_weighted
-# (-940.98065892317265, 0.0)
+#(-935.83608953817543, 0.0)
 result_weighted["pval"] = pval_weighted
 # weighted not the appropriate null distribution, but tested anyway
 result_binary["tstat"] = tstat_binary
 result_binary["pval"] = pval_binary
-# (-945.32360460633618, 0.0) - 
+# (-940.15479080240027, 0.0)
 
 pickle.dump(result_binary,open("%s/classification_results_binary_4mm.pkl" %results,"wb"))
 pickle.dump(result_weighted,open("%s/classification_results_weighted_4mm.pkl" %results,"wb"))
+
+
+# Redo the classification results with these images removed
+scores_folder = "%s/classification" %(base)
+scores = glob("%s/*.pkl" %scores_folder)
+
+# Let's save a big data frame of the prediction scores
+comparison_df = pandas.DataFrame(columns=["actual","predicted","cca_score"])
+
+total = 0
+correct = 0
+for i in range(0,len(scores)):
+    print "Parsing score %s of %s" %(i,len(scores))
+    if not re.search(missing_expression,scores[i]):
+        single_result = pickle.load(open(scores[i],"rb"))
+        total=total+2
+        correct=correct+single_result["number_correct"]
+        comparison_df = comparison_df.append(single_result["comparison_df"])
+    else:
+        print "Skipping %s" %(scores[i])
+
+# Use file names for index
+index_names = []
+for score in scores:
+    if not re.search(missing_expression,score):
+        fname = os.path.basename(score)
+        index_names = index_names + [fname,fname,fname,fname]
+
+comparison_df.index = index_names
+accuracy = correct/float(total)
+result = dict()
+result["comparison_df"] = comparison_df
+result["total"] = total
+result["correct"] = correct
+result["accuracy"] = accuracy
+pickle.dump(result,open("%s/classification_results_binary_4mm.tsv" %results,"wb"))
+#>>> accuracy
+#0.81429691583899633
