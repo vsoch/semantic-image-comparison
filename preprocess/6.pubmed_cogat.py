4251dcac93446d1a1d09664aba57c5cbdc767b17
vsoch <vsochat@stanford.edu>
2016-03-19 13:33:10 -0700

    adding mesh filter to pubmed script

--- preprocess/6.pubmed_cogat.py
+++ preprocess/6.pubmed_cogat.py
@@ -1,9 +1,10 @@
+import xmltodict # https://github.com/martinblech/xmltodict
 from cognitiveatlas.api import get_concept
-from rdflib.serializer import Serializer
-from rdflib import Graph, plugin
 from Bio import Entrez
+from glob import glob
 import pickle
-import rdfxml
+import numpy
+import pandas
 import urllib2
 import json
 import time
@@ -70,7 +71,6 @@ lookup = dict()
 counts = dict()
 
 for c in range(len(concepts["name"])):
-for c in range(745,len(concepts["name"])):
     concept = concepts["name"][c]
     terms,number_matches = search_articles(email,concept,retmax=100000)    
     if len(terms) != number_matches:
@@ -84,6 +84,101 @@ for term,pmids in lookup.iteritems():
     output_file = "%s/cogat_%s.pkl" %(pubmed_folder,term.replace(" ","_"))
     if not os.path.exists(output_file):
         print "Saving %s" %(term)
-        pickle.dump(terms,open(output_file,"wb"))
+        pickle.dump(pmids,open(output_file,"wb"))
 
 pickle.dump(counts,open("%s/cogat_COUNTS_dict.pkl" %pubmed_folder,"wb"))
+
+#################################################################################################
+# Filter Based on Mesh Terms
+#################################################################################################
+
+# @russpoldrack, semantic web ninjoid!
+# from http://docs.python-guide.org/en/latest/scenarios/xml/
+
+# Function to get treecodes from a mesh dictionary
+def get_tree_codes(doc,treecodes):
+    mesh_term_matches=[]
+    mesh_tree_matches=[]
+    for i in range(len(doc['DescriptorRecordSet']['DescriptorRecord'])):
+        descriptor = doc['DescriptorRecordSet']['DescriptorRecord'][i]
+        if not 'TreeNumberList' in descriptor.keys():
+            continue
+        for treenumber in descriptor['TreeNumberList']['TreeNumber']:
+            for treecode in treecodes:
+                if treenumber.find(treecode)==0:
+                    print descriptor['DescriptorName']['String']
+                    mesh_term_matches.append(descriptor['DescriptorName']['String'])
+                    mesh_tree_matches.append(treecode)
+    return mesh_term_matches,mesh_tree_matches
+
+
+concepts = pandas.read_csv("%s/concepts_metadata_799.tsv" %pubmed_folder,sep="\t",encoding="utf-8",index_col=0)
+mesh_file = "ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/desc2016.xml"
+response = urllib2.urlopen(mesh_file)
+mesh = response.read()
+doc = xmltodict.parse(mesh)
+treecodes=['G11.561','F','H01.158.610.030']
+mesh_term_matches,mesh_tree_matches = get_tree_codes(doc,treecodes)
+
+mesh_df = pandas.DataFrame()
+mesh_df["term_matches"] = mesh_term_matches
+mesh_df["tree_matches"] = mesh_tree_matches
+# There are repeats!
+mesh_df = mesh_df.drop_duplicates()
+# Save result to file
+mesh_df.to_csv("%s/cognitive_mesh_1119.tsv" %pubmed_folder,sep="\t")
+
+
+# Function to filter a pubmed xml by mesh term
+def filter_mesh(article_xml,mesh_filter):
+    filtered = []
+    for article in article_xml:
+        if 'MedlineCitation' in article:
+            if 'MeshHeadingList' in article['MedlineCitation']:
+                found = False
+                for mesh in article['MedlineCitation'][u'MeshHeadingList']:
+                    if found == True:
+                        continue
+                    if str(mesh["DescriptorName"]) in mesh_filter:
+                        filtered.append(str(article["MedlineCitation"]["PMID"]))
+                        found = True
+    return filtered
+
+
+# Function to fetch info on articles using efetch
+def search_mesh(email,pmids,mesh_filter,retmax=100000):
+    '''filter_mesh returns a list of articles associated with a term and count
+    :param email: an email address to associated with the query for Entrez
+    :param term: the search term for pubmed, "+" will replace spaces
+    :param retmax: maximum number of results to return, default 100K. If more exist, will be obtained.
+    '''
+    Entrez.email = email
+    iters = int(numpy.ceil(len(pmids)/float(retmax)))
+    filtered_pmids = []
+    for i in range(iters):
+        start = i*retmax
+        if (start + retmax) < len(pmids):
+            end = start + retmax
+        else:
+            end = len(pmids)
+        batch = pmids[start:end]
+        handle = Entrez.efetch(db='pubmed', id=batch, retmax=retmax,retmode="xml")
+        record = Entrez.read(handle)
+        handle.close()
+        mesh_pmids = filter_mesh(record,mesh_filter) 
+        filtered_pmids = filtered_pmids + mesh_pmids
+        time.sleep(0.5)
+    print "Found %s out of %s pmids" %(len(filtered_pmids),len(pmids))
+    return filtered_pmids
+
+# Prepare list of mesh terms
+mesh_list = mesh_df["term_matches"].tolist()
+
+# We will save each list to a file as we go
+for c in range(len(concepts["name"])):
+    concept = concepts["name"][c]
+    pmids,number_matches = search_articles(email,concept,retmax=100000)    
+    mesh_pmids = search_mesh(email,pmids,mesh_list)
+    output_file = "%s/cogat_%s_filtered.pkl" %(pubmed_folder,cogat_term.replace(" ","_"))
+    if not os.path.exists(output_file):
+        pickle.dump(mesh_pmids,open(output_file,"wb"))
