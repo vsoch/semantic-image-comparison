e2b468906afab697f0af44482101ddd23834d152
vsoch <vsochat@stanford.edu>
2016-06-06 15:03:09 -0700

    updating graph similarity scripts

--- analysis/graph/similarity.py
+++ analysis/graph/similarity.py
@@ -2,6 +2,7 @@
 # credit/thanks to networkx_addon package
 
 from collections import defaultdict
+from scipy.stats import pearsonr
 import networkx as nx
 import scipy.linalg
 import itertools
@@ -71,9 +72,27 @@ def katz(G, c=0.9, remove_neighbors=False, inv_method=0):
     S.columns = G.nodes()
     return S
 
+def dice(G):
+    '''2*bothAB/(onlyA + onlyB) + 2xbothAB'''
+    sim = pandas.DataFrame(0,columns=G.nodes(),index=G.nodes())
+    for i, a in enumerate(G.nodes()):
+        neighborsA = G.neighbors(a)
+        for b in neighborsA:
+            neighborsB = G.neighbors(b) 
+            for c in neighborsB:
+                if a == c:
+                    continue
+                s1 = set(G.neighbors(a))
+                s2 = set(G.neighbors(c))
+                bothAB = len(set.union(s1,s2))
+                sim.loc[a,c] = 2*float(bothAB) / (len(s1) + len(s2) + 2*float(bothAB))
+
+    return sim
+
+
 def ascos(G, c=0.9, max_iter=100, is_weighted=False, remove_neighbors=False, remove_self=False):
     """Return the ASCOS similarity between nodes
-    #[1] ASCOS: an Asymmetric network Structure COntext Similarity measure.
+    #[1] ASCOS: an Asymmetric network Structure Context Similarity measure.
     # Hung-Hsuan Chen and C. Lee Giles.  ASONAM 2013
     """
     node_ids = G.nodes()
@@ -154,6 +173,24 @@ def cosine(G, remove_neighbors=False):
     return cos
 
 
+def inverse_log_weighted(G):
+    A = pandas.DataFrame(nx.adjacency_matrix(G, nodelist=G.nodes(), weight=None).todense())
+    A.index = G.nodes()
+    A.columns = G.nodes()
+    N = G.number_of_nodes()
+    S = pandas.DataFrame(0,index=G.nodes(),columns=G.nodes())
+
+    degrees = G.degree()
+    for i in G.nodes():
+        A_i = A.loc[i]
+        for j in G.nodes():
+            if j != i:
+                A_j = A.loc[j]
+                intersection = A_i + A_j
+                intersection = intersection[intersection==2].index.tolist()
+                common_degrees = [1.0/numpy.log10(degrees[x]) for x in intersection]
+                S.loc[i,j] = numpy.sum(common_degrees)
+    return S 
 
 def jaccard(G, remove_neighbors=False, dump_process=False):
     jac = pandas.DataFrame()
@@ -170,3 +207,22 @@ def jaccard(G, remove_neighbors=False, dump_process=False):
                 jac.loc[a,c] = float(len(s1 & s2)) / len(s1 | s2)
     jac = jac.fillna(0)
     return jac
+
+
+# Function for RSA analysis of two matrices
+def rsa(spatial,graph):
+    # This will take the diagonal of each matrix (and the other half is changed to nan) and flatten to vector
+    vector_spatial = spatial.mask(numpy.triu(numpy.ones(spatial.shape)).astype(numpy.bool)).values.flatten()
+    vector_graph = graph.mask(numpy.triu(numpy.ones(graph.shape)).astype(numpy.bool)).values.flatten()
+    # Now remove the nans
+    vector_spatial_defined = numpy.where(~numpy.isnan(vector_spatial))[0]
+    vector_graph_defined = numpy.where(~numpy.isnan(vector_graph))[0]
+    idx = numpy.intersect1d(vector_spatial_defined,vector_graph_defined)
+    return pearsonr(vector_spatial[idx],vector_graph[idx])[0]
+
+
+def get_concepts(image1,images,contrast_lookup):
+    image1_concepts = images['cognitive_contrast_cogatlas_id'][images['image_id']==image1].tolist()[0]
+    image1_concepts = contrast_lookup.loc[image1_concepts].transpose()
+    return image1_concepts[image1_concepts!=0].index.tolist()
+
