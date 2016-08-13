772a863b217f1f9dbbd61f0a4c59c15a367a4e71
vsoch <vsochat@stanford.edu>
2016-08-13 13:05:04 -0700

    adding forward model decoder result

--- update/cca_explore/decoding_pls.py
+++ update/cca_explore/decoding_pls.py
@@ -7,6 +7,7 @@ from sklearn.cross_decomposition import PLSRegression
 from sklearn.preprocessing import StandardScaler
 from pybraincompare.mr.transformation import *
 from sklearn.metrics import r2_score, confusion_matrix
+from sklearn import linear_model
 import pickle
 import pandas
 import nibabel
@@ -26,10 +27,10 @@ number_components = int(sys.argv[7])
 X = pandas.read_csv(labels_tsv,sep="\t",index_col=0)
 
 # Transform to be -1 and 1
-for col in X.columns:
-    holder = X[col]
-    holder[holder==0] = -1
-    X[col] = holder
+#for col in X.columns:
+#    holder = X[col]
+#    holder[holder==0] = -1
+#    X[col] = holder
 
 # Save the values for later
 holdout1X = X.loc[image1_holdout,:]
@@ -82,6 +83,68 @@ del mr
 holdout1X_norm = norm.loc[image1_holdout,:]
 holdout2X_norm = norm.loc[image2_holdout,:]
 
+# BUILD THE FORWARD MODEL - elastic net
+regression_params = pandas.DataFrame(0,index=norm.columns,columns=concepts)
+
+print "Training voxels..."
+for voxel in norm.columns:
+    train = [x for x in X.index if x not in [image1_holdout,image2_holdout] and x in norm.index]
+    Y = norm.loc[train,voxel].tolist()
+    Xtrain = X.loc[train,:] 
+    # Use regularized regression
+    clf = linear_model.ElasticNet(alpha=0.1)
+    clf.fit(Xtrain,Y)
+    regression_params.loc[voxel,:] = clf.coef_.tolist()
+
+
+print "Training voxels and building predicted images..."
+for label in regression_params.columns:
+    train = [x for x in X.index if x not in [image1_holdout,image2_holdout] and x in norm.index]
+    Xtrain = norm.loc[train,:]
+    Ytrain = numpy.array(X.loc[train,:]) 
+    # Use pls instead of regularized regression
+    clf = PLSRegression(n_components=number_components)
+    # a. Use PLS to predict brains from labels instead of the elastic net i.e. the forward model pls.fit(Y_train, X_train). Note that X, and Y are flipped for the forward model.
+    clf.fit(Ytrain, Xtrain)
+    # b) Same as step b above. PLS returns a low rank weight matrix (pls.coef_), which you can use in place of W
+    regression_params.loc[voxel,:] = [x[0] for x in clf.coef_]
+    label_predictions1.loc[]
+        
+
+predictions = pandas.DataFrame(index=Xmat.index,columns=Ymat.columns)
+W = pickle.load(open("%s/regression_params_dfs.pkl" %output_folder,"rb"))['regression_params'] # [28549 
+for heldout in Xmat.index.tolist():
+    heldout_X = Xmat.loc[heldout,:] #28K by 1
+    Xtest = numpy.linalg.lstsq(W,heldout_X)[0]
+    predictions.loc[heldout,:] = Xtest
+
+
+# b) Test: estimate label scores using the forward model. With some algebra, you can show that the label scores are given by solving the linear regression problem:
+# transpose(X_test - B) = transpose(W)*transpose(Y_predict)
+Ypredict1 = clf.predict(holdout1X.reshape(1,-1))
+
+.reshape(1,-1))[0][0]
+
+regression_params.transpose().dot()
+
+#c) You can either plug the label scores directly into something like AUC, or threshold the scores to get labels. I suggested sign, but you can use whatever thresholding makes the most sense given the way the data is setup. Let's discuss this more on a separate thread if it's causing issues.
+ transpose(W)*transpose(Y_predict)
+
+
+# Need to find where regression/intercept params are in this model
+    predicted_nii1.loc[voxel,"nii"] = clf.predict(holdout1Y.reshape(1,-1))[0][0]
+    predicted_nii2.loc[voxel,"nii"] = clf.predict(holdout2Y.reshape(1,-1))[0][0]
+
+concept_vector1 =  pandas.DataFrame(holdout1Y)
+concept_vector2 =  pandas.DataFrame(holdout2Y)
+predicted_nii1 =  regression_params.dot(concept_vector1)[image1_holdout] + intercept_params["intercept"] 
+predicted_nii2 =  regression_params.dot(concept_vector2)[image2_holdout] + intercept_params["intercept"]
+
+
+c) Same as step c above using the PLS weights
+
+
+# OLD
 train = [x for x in X.index if x not in [image1_holdout,image2_holdout] and x in norm.index]
 Xtrain = norm.loc[train,:]
 Ytrain = numpy.array(X.loc[train,:]) 
